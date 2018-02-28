c2a5e4a59ee22c771b57721909fd50ecd4508722
mokhan <mohsink.khan10@gmail.com>
2018-02-28 07:27:40 -0500

    update profile vue base on pr review

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -9,31 +9,32 @@
       </h1>
       <div class="columns main-columns">
         <div class="column is-3">
-          <template v-if="changePassword">
-            <form class=change-password-form v-show="changePassword">
+          <div v-if="changePassword">
+            <div class="change-password-form">
               <ul class="password-list">
                 <li>Current Password <br><input type="password" name="old-password" v-model="oldPassword"><br></li>
                 <li>New Password <br><input type="password" name="new-password" v-model="newPassword"><br></li>
               </ul>
-              <button type="submit" name="save-password" @click="disablePasswordView(); updatePassword()" class="button is-link">Save</button>
+              <button type="submit" name="save-password" @click="updatePassword" class="button is-link">Save</button>
               <button type="reset" name="cancel-password" @click="disablePasswordView" class="button is-text">Cancel</button><br>
-            </form>
-          </template>
-          <template v-else>
-            <form class=settings-form>
+              <div class="has-text-danger">{{error}}</div>
+            </div>
+          </div>
+          <div v-else>
+            <div class="settings-form">
               <ul class="settings-list">
-                <li>First Name:<br> <input type="text" name="first-name" v-model="firstName" :disabled="!editMode"></li>
-                <li>Last Name:<br> <input type="text" name="last-name" v-model="lastName":disabled="!editMode"><br></li>
-                <li>Username:<br> <input type="text" name="username" v-model="username" :disabled="!editMode"><br></li>
-                <li>Email:<br> <input type="email" name="email" v-model="email" :disabled="!editMode"><br></li>
-                <li>Role:<br> <input type="text" name="role" v-model="role" :disabled="!editMode"><br></li>
-                <li>Mobile Number:<br> <input type="tel" name="mobile-number" v-model="mobileNumber" :disabled="!editMode"><br></li>
+                <li>Username:<br> <input type="text" name="username" v-model="user.username" disabled=true><br></li>
+                <li>Role:<br> <input type="text" name="role" v-model="user.role" disabled=true><br></li>
+                <li>First Name:<br> <input type="text" name="first-name" v-model="user.firstName" :disabled="!editMode"></li>
+                <li>Last Name:<br> <input type="text" name="last-name" v-model="user.lastName":disabled="!editMode"><br></li>
+                <li>Email:<br> <input type="email" name="email" v-model="user.email" :disabled="!editMode"><br></li>
+                <li>Mobile Number:<br> <input type="tel" name="mobile-number" v-model="user.mobileNumber" :disabled="!editMode"><br></li>
               </ul>
               <button type="submit" name="save" @click="disableEditView();updateProfile()" v-show="editMode" class="button is-link">Save</button>
               <button type="reset" name="cancel" @click="disableEditView" v-show="editMode" class="button is-text">Cancel</button><br>
               <button type="button" name="change-password" @click="enablePasswordView" class="button is-link">Change Password</button>
-            </form>
-          </template>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -44,6 +45,9 @@
   // TODO: currently allow user to fill out fields that they are not able to actually update (example username),
   //   should disable those fields even during edit view
   // TODO: implement validation for fields
+  // TODO: when a profile update fails, we currently don't reset the fields to be back to their original values. We may
+  //   want to update this behaviour so that the fields are reset back to normal. Benefit of not submitting
+  // TODO: we should reset the fields when the user clocks cancel after changing a field
 
   import ApiService from '@/services/ApiService'
 
@@ -51,16 +55,19 @@
     name: 'Profile',
     data() {
       return {
-        username : '',
-        email : '',
-        mobileNumber : '',
-        firstName : '',
-        lastName : '',
-        role : '',
+        user : {
+          username : '',
+          email : '',
+          mobileNumber : '',
+          firstName : '',
+          lastName : '',
+          role : '',
+        },
         editMode : false,
         changePassword : false,
         oldPassword : '',
-        newPassword : ''
+        newPassword : '',
+        error: null,
       }
     },
     computed: {
@@ -75,31 +82,44 @@
         ApiService.get('/user/'+this.$store.state.auth.user.id, {
           withCredentials: true
         }).then(function(response) {
-          self.username = response.data.result.username;
-          self.email = response.data.result.email;
-          self.mobileNumber = response.data.result.mobileNumber;
-          self.firstName = response.data.result.firstName;
-          self.lastName = response.data.result.lastName;
-          self.role = response.data.result.role;
+          self.user.username = response.data.result.username;
+          self.user.email = response.data.result.email;
+          self.user.mobileNumber = response.data.result.mobileNumber;
+          self.user.firstName = response.data.result.firstName;
+          self.user.lastName = response.data.result.lastName;
+          self.user.role = response.data.result.role;
         }).catch(err => console.log(err))
       },
       updateProfile() {
+        let self = this;
         const body = {
-          'email': this.email,
-          'mobileNumber': this.mobileNumber,
-          'firstName': this.firstName,
-          'lastName': this.lastName,
-          'role' : this.role,
+          'email': this.user.email,
+          'mobileNumber': this.user.mobileNumber,
+          'firstName': this.user.firstName,
+          'lastName': this.user.lastName,
+          'role' : this.user.role,
         }
         ApiService.put(
-          '/user/'+this.$store.state.auth.user.id,
+          '/user/'+self.$store.state.auth.user.id,
           body,
           { withCredentials: true }
         ).then(function(response) {
-          console.log('response = ' + JSON.stringify(response));
-        }).catch(err => console.log(err))
+          self.$toast.open({
+            message: 'Profile updated successfully',
+            type: 'is-success',
+            duration: 2000
+          })
+          self.$store.commit("login", response.data.result[0]);
+        }).catch(function (err) {
+          self.$toast.open({
+            message: 'Unable to update profile',
+            type: 'is-danger',
+            duration: 2000
+          })
+        });
       },
       updatePassword() {
+        let self = this;
         const body = {
           'oldPassword' : this.oldPassword,
           'newPassword' : this.newPassword,
@@ -110,7 +130,23 @@
           { withCredentials: true }
         ).then(function(response) {
           console.log('response = ' + JSON.stringify(response));
-        }).catch(err => console.log(err))
+
+          self.$toast.open({
+             message: 'Password updated successfully',
+             type: 'is-success',
+             duration: 2000
+           })
+
+          self.error = null;
+          self.oldPassword = null;
+          self.newPassword = null;
+          self.disablePasswordView();
+        }).catch(err => {
+
+          console.log(err)
+          self.error = "Incorrect password entered."
+
+        })
       },
       enableEditView() {
         this.disablePasswordView();
@@ -142,10 +178,8 @@
         font-weight: bold;
       }
     }
-    form{
-      button {
+    button {
         margin-bottom: 0.75rem;
-      }
     }
   }
 </style>

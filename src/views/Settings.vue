b5e9cdb243361e729ebc5ca76d54ee0ffdb216d0
Rajaie AlKorani <rajaie.alkorani@gmail.com>
2018-03-27 23:08:04 -0400

    CV-15 address PR comments

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,47 +3,47 @@
     <div class="container is-fluid">
       <h1 class="title">Settings</h1>
       <div class="main">
-        <!-- Method -->
+        <!-- Type -->
         <nav class="panel column is-3 notifications">
           <p class="panel-heading">
             Notifications
           </p>
-          <div class="panel-block">
-            <h3 class="is-4">Method</h3>
+          <div class="panel-block settings-subheader">
+            <h3 class="is-4">Type</h3>
               <b-tooltip label="How would you like us to notify you?"
                          position="is-right">
                   <span class="icon"><i class="fas fa-info-circle"></i></span>
               </b-tooltip>
           </div>
-          <label class="panel-block">
+          <label class="panel-block settings-option">
             <input type="checkbox" v-model="userSettings.emailNotifications">
             Email
           </label>
-          <label class="panel-block">
+          <label class="panel-block settings-option">
             <input v-model="userSettings.smsNotifications" type="checkbox">
             SMS
           </label>
           <!-- Events -->
-          <div class="panel-block">
+          <div class="panel-block settings-subheader">
             <h3 class="is-4">Events</h3>
               <b-tooltip label="What would you like to be notified of?"
                          position="is-right">
                   <span class="icon"><i class="fas fa-info-circle"></i></span>
               </b-tooltip>
           </div>
-          <label class="panel-block">
+          <label class="panel-block settings-option">
             <input v-model="userSettings.newAppointmentNotifications" type="checkbox">
             New Appointments
           </label>
-          <label class="panel-block">
+          <label class="panel-block settings-option">
             <input v-model="userSettings.rescheduledAppointmentNotifications" type="checkbox">
             Re-scheduled Appointments
           </label>
-          <label class="panel-block">
+          <label class="panel-block settings-option">
             <input v-model="userSettings.cancelledAppointmentNotifications" type="checkbox">
             Cancelled Appointments
           </label>
-          <label class="panel-block">
+          <label class="panel-block settings-option">
             <input v-model="userSettings.appointmentReminderNotifications" type="checkbox">
             Appointment Reminders
             <b-tooltip label="Get an appointment reminder 24 hours before your appointment"
@@ -54,6 +54,9 @@
           <div class="panel-block">
             <button @click="updateSettings" class="button is-link is-outlined is-fullwidth">
               update settings
+            </button>
+            <button @click="resetSettings" class="button is-text">
+              cancel
             </button>
           </div>
         </nav>
@@ -69,7 +72,8 @@
     name: 'Appointments',
     data() {
       return {
-        userSettings: {}
+        userSettings: {},
+        userSettingsCopy: {}
       }
     },
     computed: {
@@ -78,6 +82,7 @@
       try {
         const res = await ApiService.get('/user/settings')
         this.userSettings = Object.assign({}, res.data.result)
+        this.userSettingsCopy = Object.assign({}, res.data.result)
       }
       catch(err) {
         console.log(err)
@@ -88,16 +93,20 @@
         try {
           const res = await ApiService.patch('/user/settings', this.userSettings)
           this.userSettings = Object.assign({}, res.data.result)
+          this.userSettingsCopy = Object.assign({}, res.data.result)
 
           this.$toast.open({
             message: 'Your preferences have been updated successfully!',
             type: 'is-success',
-            duration: 2000
+            duration: 3000
           })
         }
         catch(err) {
           console.log(err)
         }
+      },
+      resetSettings() {
+        this.userSettings = Object.assign({}, this.userSettingsCopy)
       }
     }
   }
@@ -109,6 +118,16 @@
     padding-top: 20px;
     .main {
       margin-top: 1rem;
+    }
+
+    .panel-heading {
+      background: #e5ffe2;
+    }
+    .settings-subheader {
+      background: whitesmoke;
+    }
+    .settings-option {
+      padding-left: 2rem;
     }
   }
 </style>
